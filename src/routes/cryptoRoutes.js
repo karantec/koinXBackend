1ec816ce1ccc1eeec96c3456684132eca6d084c6
karantec <karanrana3095@gmail.com>
2024-10-10 14:11:20 +0530

    finalcommit

--- src/routes/cryptoRoutes.js
+++ src/routes/cryptoRoutes.js
@@ -8,21 +8,5 @@ router.post('/update',updateCryptoData);
 router.get('/stats', getStats);
 router.get('/deviation', getDeviation);
 
-// New debug endpoint
-router.get('/debug', async (req, res) => {
-  try {
-    const counts = await CryptoCurrency.aggregate([
-      { $group: { _id: '$coinId', count: { $sum: 1 } } }
-    ]);
-    const latestRecords = await CryptoCurrency.find().sort({ createdAt: -1 }).limit(3);
-    res.json({ 
-      message: 'Debug information',
-      counts: counts,
-      latestRecords: latestRecords 
-    });
-  } catch (error) {
-    res.status(500).json({ error: error.message });
-  }
-});
 
 module.exports = router;
